7ed05552641dbad4900918edf31e391618318dc5
mqudah <mohghq@gmail.com>
2024-07-06 13:49:28 +0300

    add rust_analyzer

--- nvim/lua/core/plugin_config/lsp.lua
+++ nvim/lua/core/plugin_config/lsp.lua
@@ -1,30 +1,24 @@
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-
-local util = require('lspconfig/util')
-local path = util.path
-
-local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-  -- Find and use virtualenv in workspace directory.
-  for _, pattern in ipairs({'*', '.*'}) do
-    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
-    if match ~= '' then
-      return path.join(path.dirname(match), 'bin', 'python')
-    end
-  end
-  -- Fallback to system Python.
-  return exepath('python3') or exepath('python') or 'python'
-end
+require('lspconfig').rust_analyzer.setup({
+  settings = {
+    ['rust-analyzer'] = {
+      diagnostics = {
+        enable = false;
+      }
+    }
+  }
+})
 
 require('lspconfig').pyright.setup({
-  before_init = function(_, config)
-    config.settings.python.pythonPath = get_python_path(config.root_dir)
-  end
+  --before_init = function(_, config)
+  --  config.settings.python.pythonPath = get_python_path(config.root_dir)
+  --end,
+	venvPath = ".",
+	venv = ".venv"
 })
+
+require('lspconfig').tsserver.setup({})
 
 -- make sure to disable diagnostics and ignore vendor directory to improve performance
 -- `$ phpactor config:set language_server.diagnostics_on_update false`
